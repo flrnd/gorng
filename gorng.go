f01f3cfb2ca208589f8a7ff8c89648672483e573
Florian Rand <florian.rand@gmail.com>
2022-02-14 13:04:05 +0100

    first commit

---
+++ gorng.go
@@ -0,0 +1,29 @@
+package gorng
+
+import (
+	crand "crypto/rand"
+	"encoding/binary"
+	mrand "math/rand"
+)
+
+var rng = randInit()
+
+type cryptoSource struct{}
+
+func (s *cryptoSource) Seed(seed int64) {}
+
+func (s *cryptoSource) Uint64() (value uint64) {
+	binary.Read(crand.Reader, binary.BigEndian, &value)
+	return value
+}
+
+func (s *cryptoSource) Int63() int64 {
+	return int64(s.Uint64() & ^uint64(1<<63))
+}
+
+func randInit() (myRand *mrand.Rand) {
+	var src mrand.Source64 = &cryptoSource{}
+	myRand = mrand.New(src)
+
+	return myRand
+}
